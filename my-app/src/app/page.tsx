6341571c8142a98b1acbe04433a4be3a48b6d195
Vollmondo <dmitrifom74@gmail.com>
2023-12-15 11:56:19 +0500

    first steps. learning to render and fetch

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,20 +1,38 @@
-import Image from 'next/image'
-import { Header } from './components/Header'
+"use client"
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+async function fetchData() {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const result = await res.json();
+    return result;
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+    return [];
+  }
+}
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData().then(result => {
+      setData(result);
+    });
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-    </main>
-  )
+    <div>
+      <h1>Главная страница</h1>
+      {data.map((el: { id: string, title:string, body:string}) => (
+        <div key={el.id} className='post'>
+          <h2>{el.title}</h2>
+          <p>{el.body}</p>
+          <Link href={`/post/${el.id}`}>Подробнее...</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
